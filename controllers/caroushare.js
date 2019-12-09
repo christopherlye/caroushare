4bb8b8573eb9c661ececa6894242c09a5b67cbe1
Shahrani <shahranibinrahiman@gmail.com>
2019-12-09 19:26:34 +0800

    books.js created form to input into

--- controllers/caroushare.js
+++ controllers/caroushare.js
@@ -1,34 +1,29 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Books = require("../models/books.js");
+const Books = require('../models/books.js');
 
-router.get("/", (req, res) => {
-  Books.find({}, (err, foundBooks) => {
-    res.json(foundBooks);
-  });
+router.get('/', (req, res) => {
+	Books.find({}, (err, foundBooks) => {
+		res.json(foundBooks);
+	});
 });
 
-router.post("/", (req, res) => {
-  Books.create(req.body, (err, createdBook) => {
-    res.json(createdBook); //.json() will send proper headers in response so client knows it's json coming back
-  });
+router.post('/books', (req, res) => {
+	Books.create(req.body, (err, createdBook) => {
+		res.json(createdBook); //.json() will send proper headers in response so client knows it's json coming back
+	});
 });
 
-router.delete("/:id", (req, res) => {
-  Books.findByIdAndRemove(req.params.id, (err, deletedBook) => {
-    res.json(deletedBook);
-  });
+router.delete('/:id', (req, res) => {
+	Books.findByIdAndRemove(req.params.id, (err, deletedBook) => {
+		res.json(deletedBook);
+	});
 });
 
-router.put("/:id", (req, res) => {
-  Books.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true },
-    (err, updatedBook) => {
-      res.json(updatedBook);
-    }
-  );
+router.put('/:id', (req, res) => {
+	Books.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedBook) => {
+		res.json(updatedBook);
+	});
 });
 
 module.exports = router;
--- public/js/components/books.js
+++ public/js/components/books.js
@@ -1,12 +1,83 @@
 class Books extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title  : '',
+			author : '',
+			image  : '',
+			books  : []
+		};
+	}
+
+	//Component did mount
+	componentDidMount = () => {
+		fetch('/books').then((response) => response.json()).then((books) => {
+			this.setState({ books: books });
+		});
+	};
+
+	//handle change and submit
+	handleChange = (event) => {
+		this.setState({ [event.target.id]: event.target.value });
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(this.state);
+		fetch('/books', {
+			body    : JSON.stringify({ title: this.state.title }),
+			method  : 'POST',
+			headers : {
+				Accept         : 'application/json, text/plain, */*',
+				'Content-Type' : 'application/json'
+			}
+		})
+			.then((createdBook) => {
+				return createdBook.json();
+			})
+			.then((jsonedBook) => {
+				// reset the form
+				// add person to list
+				this.setState({
+					title : '',
+					books : [jsonedBook, ...this.state.books]
+				});
+				console.log(books);
+			})
+			.catch((error) => console.log(error));
+	};
+
 	render() {
 		return (
 			<React.Fragment>
-                <Navbar />
-                <br />
+				<Navbar />
 				<br />
 				<br />
-				<h6>Books Display</h6>
+				<br />
+				<h6>Books Catalogue</h6>
+				<form onSubmit={this.handleSubmit}>
+					<label for="title" />
+					<input
+						type="text"
+						placeholder="title"
+						value={this.state.title}
+						onChange={this.handleChange}
+						id="title"
+					/>
+					<input type="submit" value="Create Book!" />
+				</form>
+				{/* <ul>
+					{this.state.books.map((book) => {
+						return (
+							<ul>
+								<li>{book.title}</li>
+								<li>{book.image}</li>
+								<td onClick={() => this.deleteBook(book._id, index)}> X </td>
+								<td onClick={() => this.updateBook(book, index)}> Edit Book </td>
+							</ul>
+						);
+					})}
+				</ul> */}
 			</React.Fragment>
 		);
 	}
