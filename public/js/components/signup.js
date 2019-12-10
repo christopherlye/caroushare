b3a81d1d15a6ce64cfb83f0dcb5980f867b3d14f
Shahrani <shahranibinrahiman@gmail.com>
2019-12-10 20:43:25 +0800

    files

---
+++ public/js/components/signup.js
@@ -0,0 +1,50 @@
+class Signup extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<Navbar />
+				<br />
+				<br />
+				<br />
+				<h6>Log in or sign up page</h6>
+
+				<div className="container card main">
+					<img src="../img/login.jpg" className="card-img-top" alt="Caroushare log in" />
+					<div className="card-body">
+						{/* To edit the action tag */}
+						<form className="col s12 form-signin" action="" method="POST">
+							<h1 className="h3 mb-3 font-weight-normal">Sign Up</h1>
+							<input
+								placeholder="username"
+								id="username"
+								type="text"
+								className="validate"
+								name="username"
+								required
+							/>
+							<label for="username" className="sr-only">
+								User Name
+							</label>
+							<br />
+							<input
+								placeholder="password"
+								id="password"
+								type="password"
+								className="validate"
+								name="password"
+								required
+							/>
+							<label for="password" className="sr-only">
+								Password
+							</label>
+							<br />
+							<button className="btn waves-effect waves-light" type="submit" name="action">
+								Submit
+							</button>
+						</form>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
