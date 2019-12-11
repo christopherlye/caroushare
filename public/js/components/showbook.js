829e00dfa1315934d796220c1075a44bcd34d381
Zachary Loke <zacharylky@gmail.com>
2019-12-11 20:22:41 +0800

    trying to fix show page

--- public/js/components/showbook.js
+++ public/js/components/showbook.js
@@ -9,25 +9,36 @@ class Showbook extends React.Component {
     };
   }
 
-  async componentDidMount() {
-    console.log("running");
-    let bookStore = await fetch("/books");
-    console.log(bookStore.title);
-    console.log(bookStore);
-    this.setState(state => {
-      title: bookStore.title;
-    });
-  }
+  componentDidMount = event => {
+    fetch("/books")
+      .then(response => response.json())
+      .then(books => {
+        this.setState({ books: books });
+      });
+    // console.log(this.state.books);
+  };
+
+  //   async componentDidMount() {
+  //     console.log("running");
+  //     let bookStore = await fetch("/books");
+  //     console.log(bookStore.title);
+  //     console.log(bookStore);
+  //     this.setState(state => {
+  //       title: bookStore.title;
+  //     });
+  //   }
 
   render() {
     return (
       <React.Fragment>
         <Navbar />
         <div>
-          {this.state.books.map(book => {
-            <tr>
-              <td>{book.title}</td>
-            </tr>;
+          {this.state.books.map((book, index) => {
+            return (
+              <tr>
+                <td>{book.title}</td>
+              </tr>
+            );
           })}
         </div>
 
