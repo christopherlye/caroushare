6ec8610d811c33b1eed4675ec9b23ca1df59dcb7
christopherlye <christopherlye91@gmail.com>
2019-12-17 17:21:11 +0800

    chris updates

--- public/js/components/navbar.js
+++ public/js/components/navbar.js
@@ -1,89 +1,89 @@
 class Navbar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currentUser : this.props.currentUser
-		};
-	}
-	render() {
-		return (
-			<nav className="navbar navbar-expand-md navbar-dark fixed-top">
-				<a className="navbar-brand" href="/">
-					Caroushare
-				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarsExampleDefault"
-					aria-controls="navbarsExampleDefault"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon" />
-				</button>
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: this.props.currentUser
+    };
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top">
+        <a className="navbar-brand" href="/">
+          Caroushare
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExampleDefault"
+          aria-controls="navbarsExampleDefault"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
 
-				{/* Navigation b  */}
-				<div className="collapse navbar-collapse" id="navbarsExampleDefault">
-					<ul className="navbar-nav ml-auto">
-						<li className="nav-item">
-							<Link to="/" className="nav-link">
-								Home
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/about" className="nav-link">
-								About
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/books" className="nav-link">
-								Books
-							</Link>
-						</li>
-						{/* To profile button  */}
-						{this.props.currentUser ? (
-							<li className="nav-item">
-								<Link to="/profile" className="nav-link">
-									Profile
-								</Link>
-							</li>
-						) : (
-							''
-						)}
+        {/* Navigation b  */}
+        <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/books" className="nav-link">
+                Books
+              </Link>
+            </li>
+            {/* To profile button  */}
+            {this.props.currentUser ? (
+              <li className="nav-item">
+                <Link to="/profile" className="nav-link">
+                  Profile
+                </Link>
+              </li>
+            ) : (
+              ""
+            )}
 
-						{/* To toggle log in or log out button  */}
-						{this.props.currentUser ? (
-							<li className="nav-item nav-link" onClick={this.props.toLogout}>
-								<Link to="/">Logout</Link>
-							</li>
-						) : (
-							<li className="nav-item">
-								<Link to="/login" className="nav-link">
-									Log In
-								</Link>
-							</li>
-						)}
+            {/* To toggle log in or log out button  */}
+            {this.props.currentUser ? (
+              <li className="nav-item nav-link" onClick={this.props.toLogout}>
+                <Link to="/">Logout</Link>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  Log In
+                </Link>
+              </li>
+            )}
 
-						{/* for signup */}
-						{this.props.currentUser ? (
-							''
-						) : (
-							<li className="nav-item">
-								<Link to="/signup" className="nav-link">
-									Sign Up
-								</Link>
-							</li>
-						)}
+            {/* for signup */}
+            {this.props.currentUser ? (
+              ""
+            ) : (
+              <li className="nav-item">
+                <Link to="/signup" className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            )}
 
-						{/* <li className="nav-item">
+            {/* <li className="nav-item">
 							<Link to="/profile" className="nav-link">
 								My Profile
 							</Link>
 						</li> */}
-					</ul>
-				</div>
-			</nav>
-		);
-	}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
--- public/js/components/profile.js
+++ public/js/components/profile.js
@@ -1,154 +1,172 @@
 class Profile extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			username    : '',
-			currentUser : this.props.currentUser,
-			title       : '',
-			author      : '',
-			image       : '',
-			books       : []
-		};
-	}
-	//Component did mount
-	componentDidMount = () => {
-		fetch('/books').then((response) => response.json()).then((books) => {
-			this.setState({ books: books });
-		});
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      currentUser: this.props.currentUser,
+      title: "",
+      author: "",
+      image: "",
+      books: []
+    };
+  }
+  //Component did mount
+  componentDidMount = () => {
+    fetch("/books")
+      .then(response => response.json())
+      .then(books => {
+        this.setState({ books: books });
+      });
+  };
 
-	//handle change and submit
-	handleChange = (event) => {
-		this.setState({ [event.target.id]: event.target.value });
-	};
+  //handle change and submit
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
 
-	handleSubmit = (event) => {
-		event.preventDefault();
-		console.log(this.state);
-		fetch('/books', {
-			body    : JSON.stringify({ title: this.state.title }),
-			method  : 'POST',
-			headers : {
-				Accept         : 'application/json, text/plain, */*',
-				'Content-Type' : 'application/json'
-			}
-		})
-			.then((createdBook) => {
-				return createdBook.json();
-			})
-			.then((jsonedBook) => {
-				// reset the form
-				// add book to list
-				this.setState({
-					title : '',
-					books : [jsonedBook, ...this.state.books]
-				});
-				console.log(books);
-			})
-			.catch((error) => console.log(error));
-	};
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    fetch("/books", {
+      body: JSON.stringify({ title: this.state.title }),
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(createdBook => {
+        return createdBook.json();
+      })
+      .then(jsonedBook => {
+        // reset the form
+        // add book to list
+        this.setState({
+          title: "",
+          books: [jsonedBook, ...this.state.books]
+        });
+        console.log(books);
+      })
+      .catch(error => console.log(error));
+  };
 
-	//Delete Book
-	deleteBook(id, index) {
-		fetch('/books/' + id, {
-			method : 'DELETE'
-		}).then((data) => {
-			this.setState({
-				books : [...this.state.books.slice(0, index), ...this.state.books.slice(index + 1)]
-			});
-		});
-	}
+  //Delete Book
+  deleteBook(id, index) {
+    fetch("/books/" + id, {
+      method: "DELETE"
+    }).then(data => {
+      this.setState({
+        books: [
+          ...this.state.books.slice(0, index),
+          ...this.state.books.slice(index + 1)
+        ]
+      });
+    });
+  }
 
-	//Update Book
-	updateBook(book, index) {
-		fetch('/books' + book._id, {
-			body    : JSON.stringify(book),
-			method  : 'PUT',
-			headers : {
-				// Accept         : 'application/json, text/plain, */*',
-				'Content-Type' : 'application/json'
-			}
-		})
-			.then((updatedBook) => updatedBook.json())
-			.then((jsonedBook) => {
-				fetch('/books').then((response) => response.json()).then((books) => {
-					this.setState({ books: books });
-				});
-			});
-	}
+  //Update Book
+  updateBook(book, index) {
+    fetch("/books" + book._id, {
+      body: JSON.stringify(book),
+      method: "PUT",
+      headers: {
+        // Accept         : 'application/json, text/plain, */*',
+        "Content-Type": "application/json"
+      }
+    })
+      .then(updatedBook => updatedBook.json())
+      .then(jsonedBook => {
+        fetch("/books")
+          .then(response => response.json())
+          .then(books => {
+            this.setState({ books: books });
+          });
+      });
+  }
 
-	///////////////////////////////////Render
-	render() {
-		return (
-			<React.Fragment>
-				{/* <Navbar /> */}
-				<br />
-				<br />
-				<br />
-				<div className="container profileContainer">
-					<div className="row">
-						<h1>My Profile</h1>
-					</div>
-					{/* Display Container Area */}
-					<div class="container-fluid card main ">
-						<div class="card-body">
-							{console.log('user:', this.state.currentUser.username)}
-							<h1>Hi {this.state.currentUser.username}!</h1>
+  ///////////////////////////////////Render
+  render() {
+    return (
+      <React.Fragment>
+        {/* <Navbar /> */}
+        <br />
+        <br />
+        <br />
+        <div className="container profileContainer">
+          <div className="row">
+            <h1>My Profile</h1>
+          </div>
+          {/* Display Container Area */}
+          <div class="container-fluid card main ">
+            <div class="card-body">
+              {console.log("user:", this.state.currentUser.username)}
+              <h1>Hi {this.state.currentUser.username}!</h1>
 
-							<h4>What would you like to do today?</h4>
-							<Link to="/newbook" className="btn btn-secondary btn-lg btn-custom">
-								Add new books
-							</Link>
-							<br />
-							<br />
-							<p>These are your current listings:</p>
-							{/* Show all only my books */}
+              <h4>What would you like to do today?</h4>
+              <Link
+                to="/newbook"
+                className="btn btn-secondary btn-lg btn-custom"
+              >
+                Add new books
+              </Link>
+              <br />
+              <br />
+              <p>These are your current listings:</p>
+              {/* Show all only my books */}
 
-							<div class="row">
-								{this.state.books.map((book, index) => {
-									return this.state.currentUser._id === book.user._id ? (
-										<div className="col-4">
-											{/* {console.log(book)} */}
-											<Link
-												to={{
-													pathname : '/showbook',
-													state    : {
-														book : book
-													}
-												}}
-											>
-												<img src={book.image} className="img-fluid img-thumb shadow" />
-												<h6>{book.title}</h6>
-											</Link>
-											<p>{book.author}</p>
-											<Link to="/toeditbooks">Edit Listing</Link>
-											<p onClick={() => this.deleteBook(book._id, index)}>Delete Listing</p>
-										</div>
-									) : (
-										''
-									);
-								})}
-							</div>
-							<div class="buttonContainer">
-								<Link to="/newbook" className="btn btn-secondary btn-lg btn-custom">
-									Add new books
-								</Link>
+              <div class="row">
+                {this.state.books.map((book, index) => {
+                  return this.state.currentUser._id === book.user._id ? (
+                    <div className="col-4">
+                      {/* {console.log(book)} */}
+                      <Link
+                        to={{
+                          pathname: "/showbook",
+                          state: {
+                            book: book
+                          }
+                        }}
+                      >
+                        <img
+                          src={book.image}
+                          className="img-fluid img-thumb shadow"
+                        />
+                        <h6>{book.title}</h6>
+                      </Link>
+                      <p>{book.author}</p>
+                      <Link to="/toeditbooks">Edit Listing</Link>
+                      <p onClick={() => this.deleteBook(book._id, index)}>
+                        Delete Listing
+                      </p>
+                    </div>
+                  ) : (
+                    ""
+                  );
+                })}
+              </div>
+              <div class="buttonContainer">
+                <Link
+                  to="/newbook"
+                  className="btn btn-secondary btn-lg btn-custom"
+                >
+                  Add new books
+                </Link>
 
-								{/* <Link
+                {/* <Link
 									to="/"
 									className="btn btn-secondary btn-lg btn-custom"
 									onClick={this.props.toLogout}
 								>
 									Log Out
 								</Link> */}
-								{/* <button className="nav-item nav-link" onClick={this.props.toLogout}>
+                {/* <button className="nav-item nav-link" onClick={this.props.toLogout}>
 									<Link to="/">Logout</Link>
 								</button> */}
-							</div>
-						</div>
-					</div>
-				</div>
-			</React.Fragment>
-		);
-	}
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
