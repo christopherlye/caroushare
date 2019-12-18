297354d3ee2a7e54c57cb440922747e0cf4e1cea
Shahrani <shahranibinrahiman@gmail.com>
2019-12-18 15:49:18 +0800

    wrong password working already

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -6,11 +6,22 @@ const User = require('../models/users.js');
 
 sessions.post('/', (req, res) => {
 	User.findOne({ username: req.body.username }, (err, foundUser) => {
-		if (err) console.log(err.message);
-		if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-			req.session.currentUser = foundUser;
-			res.json(foundUser);
+		if (err) {
+			console.log(err.message);
+			res.send('Error loading mongo');
 		}
+		console.log('found: ', foundUser);
+		if (foundUser == null) {
+			res.send({ message: null });
+		} else {
+			if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+				req.session.currentUser = foundUser;
+				res.json(foundUser);
+			} else {
+				res.send({ message: false });
+			}
+		}
+		w;
 	});
 });
 
